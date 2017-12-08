720cc136220a1a9ebc85409d0d1d086d3e5cec75
Joel Field <joelmatthewfield@outlook.com>
2017-12-08 10:20:39 +1000

    start from scratch

--- app.js
+++ app.js
@@ -7,57 +7,14 @@ new Vue({
 
   computed: {
 
-    toggleEditClass: function(){
-      return this.editingUsers ? "editing_users" : "";
-    },
-
-    filterReturnedNoUsers: function(){
-      return this.$eval("users | filterBy filterUser").length === 0;
-    }
-
   },
 
   data: function(){
     return {
-      users: [
-        {
-            first_name: "Joel",
-            last_name: "Field",
-            dob: "22/02/1992",
-        },
-        {
-            first_name: "Zach",
-            last_name: "Kominar",
-            dob: "22/02/1850",
-        },
-        {
-            first_name: "Mark",
-            last_name: "Zuckerberg",
-            dob: "22/02/1980",
-        },
-        {
-            first_name: "Elon",
-            last_name: "Musk",
-            dob: "22/02/1500",
-        },
-      ],
-      editingUsers: false,
     }
   },
 
   methods: {
 
-    removeUser: function(user){
-      this.users.$remove(user);
-    },
-
-    addUser: function(){
-      this.users.push({
-        first_name: "",
-        last_name: "",
-        dob: "15/08/1990"
-      });
-
-    }
   }
 })
