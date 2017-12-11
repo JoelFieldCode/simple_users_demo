58ad7264d19be890444dffe9d95029686154414e
Joel Field <joelmatthewfield@outlook.com>
2017-12-11 10:26:51 +1000

    scratch from scratch demo

--- user.js
+++ user.js
@@ -1,32 +1,12 @@
 var $$user = Vue.extend({
 
-  props: ['user'],
+  props: [],
 
   computed: {
-
-    fullName: function(){
-      if(this.user.first_name === "" && this.user.last_name === ""){
-        return "Empty User";
-      }
-
-      return this.user.first_name + " " + this.user.last_name;
-    },
-
-    age: function(){
-
-      var age = moment().diff(moment(this.user.dob, 'DD/MM/YYYY'), 'years');
-
-      if(isNaN(age)){
-        return "Invalid Date entered";
-      }
-      return age;
-
-    }
 
   },
 
   template: `
-      {{fullName}} (<i>age {{age}}</i>)
   `
-  
+
 })
